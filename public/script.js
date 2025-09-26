e158cae2c637f3d7ab0ec467da4082c419901c27
hassanfa900 <Hfa.900@gmail.com>
2025-09-26 06:47:12 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,25 +1,21 @@
-async function fetchAramco() {
-    try {
-        const res = await fetch('/api/aramco');
-        const data = await res.json();
-        const tbody = document.querySelector('#stock-table tbody');
-
-        tbody.innerHTML = `
-            <tr>
-                <td>${data.name}</td>
-                <td>${data.symbol}</td>
-                <td>${data.price}</td>
-                <td>${data.change}</td>
-                <td>${data.percent}</td>
-                <td>${data.volume}</td>
-                <td>${data.signal}</td>
-            </tr>
-        `;
-    } catch (err) {
-        console.error(err);
-    }
-}
-
-// تحديث كل 30 ثانية
-fetchAramco();
-setInterval(fetchAramco, 30000);
+fetch('/api/aramco')
+  .then(res => res.json())
+  .then(data => {
+    const tbody = document.getElementById('stock-data');
+    tbody.innerHTML = `
+      <tr>
+        <td>${data.name}</td>
+        <td>${data.symbol}</td>
+        <td>${data.price}</td>
+        <td>${data.change}</td>
+        <td>${data.volume}</td>
+        <td>${data.ema9.toFixed(2)}</td>
+        <td>${data.ema20.toFixed(2)}</td>
+        <td>${data.rsi}</td>
+        <td>${data.macdSignal}</td>
+        <td>${data.mfi}</td>
+        <td>${data.entrySignal}</td>
+      </tr>
+    `;
+  })
+  .catch(err => console.error(err));
